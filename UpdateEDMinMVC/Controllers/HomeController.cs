73f1a6f27ef4903d77f90151fefb7018aecf8850
idrees04 <idreesar@yahoo.com>
2020-08-21 17:57:09 +0500

    Add project files.

---
+++ UpdateEDMinMVC/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UpdateEDMinMVC.Models;
+
+namespace UpdateEDMinMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        EDMExampleMVCEntities db = new EDMExampleMVCEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = db.students.ToList();
+
+            return View(data);
+        }
+
+        public ActionResult Employee()
+        {
+            var data = db.employees.ToList();
+
+            return View(data);
+        }
+    }
+}
